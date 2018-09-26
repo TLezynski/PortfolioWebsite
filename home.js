b9027596e5374d9910f67c55c544f5f43a02144d
Trevor Lezynski <tdog8095@yahoo.com>
2018-09-25 23:05:31 -0400

    Finish fixing javascript window.scrollTo

---
+++ home.js
@@ -0,0 +1,16 @@
+
+function scollToAbout(){
+  window.scroll({
+  top: 1000,
+  left: 0,
+  behavior: 'smooth'
+});
+}
+
+function scollToProject(){
+  window.scrollTo({
+  top: document.getElementById("Projects"),
+  left: 0,
+  behavior: 'smooth'
+});
+}
