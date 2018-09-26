9d5a2619516c14c0874d0343719c5cee5f9ab634
Trevor Lezynski <tdog8095@yahoo.com>
2018-09-26 19:16:52 -0400

    Smooth scrolling functionality implemented.

--- home.js
+++ home.js
@@ -1,16 +1,14 @@
 
-function scollToAbout(){
-  window.scroll({
-  top: 1000,
-  left: 0,
+function scrollToElement(id){
+  document.getElementById('Projects');
+  var topPos = document.getElementById(id).offsetTop;
+
+  window.scrollTo({
+  top: topPos,
   behavior: 'smooth'
-});
+  });
 }
 
-function scollToProject(){
-  window.scrollTo({
-  top: document.getElementById("Projects"),
-  left: 0,
-  behavior: 'smooth'
-});
+function test(){
+  window.scrollBy(0, 50);
 }
